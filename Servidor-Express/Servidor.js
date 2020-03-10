85ddd99d6bed11711d29fbd22a24728806a1c7a2
MATHEUS PAIVA ANDRADE BRITO <365191020@labinfo.unifacs.br>
2020-03-10 14:24:03 -0300

    Inicio

--- Servidor-Express/Servidor.js
+++ Servidor-Express/Servidor.js
@@ -1,2 +1,15 @@
 const express = require('express')
 const server = express()
+
+server.get('/', (req, res) =>{
+    res.send('<h1>Casa</h1>')
+})
+
+server.get('/Home', (req, res) =>{
+    res.send('<h1>Home</h1>')
+})
+
+server.listen(3000, () =>{
+    console.log('Servidor hospedado em http://localhost:3000')
+    console.log('Para derrubar o servidor: ctrl + c')
+})
